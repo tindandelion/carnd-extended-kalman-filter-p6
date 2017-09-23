cba3697871c128aea57e3dfefe5c7c94db0c64e5
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-09-23 16:38:38 +0300

    Convert radar coordinates to cartesian

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -138,7 +138,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    cout << "Radar data = " << endl << measurement_pack.raw_measurements_ << endl;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
