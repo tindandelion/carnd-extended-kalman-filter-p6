948693a86f7ea20f54d054dedb40b4f8a3443fb0
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-09-27 07:39:16 +0300

    Calculating error y for radar estimates

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,9 +1,24 @@
 #include "kalman_filter.h"
+#include <iostream>
+#include <cmath>
 
+using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 static MatrixXd I = MatrixXd::Identity(4, 4);
+
+VectorXd h(const VectorXd& x) {
+  double px = x[0], py = x[1], vx = x[2], vy = x[3];
+  VectorXd res(3);
+  
+  double rho = sqrt(px*px + py*py);
+  double phi = atan2(py, px);
+  double rhodot = (px*vx + py*vy) / rho;
+  
+  res << rho, phi, rhodot;
+  return res;
+}
 
 KalmanFilter::KalmanFilter() {}
 
@@ -33,7 +48,8 @@ void KalmanFilter::Update(const VectorXd &z) {
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  
+  VectorXd y = z - h(x_);
+  cout << "y = " << endl << y << endl;
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
