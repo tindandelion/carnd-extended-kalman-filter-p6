120442eb5b7444d1fbe441286c4bf558a8bc4d86
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-09-30 18:04:19 +0300

    Final touches

--- src/measurement_models.hpp
+++ src/measurement_models.hpp
@@ -3,6 +3,14 @@
 
 #include "motion_model.hpp"
 
+class NullMeasurementModel: public MeasurementModel {
+public:
+  void InitState(const VectorXd& measurement, VectorXd& state) const {
+    state << 0.1, 0.1, 0, 0;
+  }
+  void UpdateState(const VectorXd& measurement, VectorXd& state, MatrixXd& cov) const { }
+};
+
 class LaserMeasurementModel: public MeasurementModel {
 private:
   MatrixXd R;
